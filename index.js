1cb3575e3c8ae2bc6d3829ab04e194b00474ff41
JmzTradehacker <japhet.mdn@gmail.com>
2024-10-09 20:06:58 -0700

    fixed syntax error in import

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Importing database functions. DO NOT MODIFY THIS LINE.
-import { central, db1, db2, db3, vault } from "./databases.js";
+import { central, db1, db2, db3, vault } from "./database.js";
 
 // function getUserData(id) {
 //   const dbs = {
